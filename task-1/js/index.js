e5374404e4be94e692a061ee3e570f509af0e129
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-02-18 16:29:35 +0300

    Adds javascript code

--- task-1/js/index.js
+++ task-1/js/index.js
@@ -1,2 +1,40 @@
 'use strict';
 
+const fighter1 = {
+  name: 'John',
+  damage: damage,
+  life: 10000000,
+};
+
+const fighter2 = {
+  name: 'Mike',
+  damage: damage,
+  life: 10000000,
+};
+
+function damage() {
+  this.life -= Math.random();
+  if (this.life < 0) this.life = 0;
+}
+
+function fight(person1, person2) {
+  const text = 'Бой окончен, победитель:';
+
+  console.log('Идет бой...');
+
+  while (true) {
+    person1.damage();
+    if (person1.life == 0) {
+      console.log(`${text} ${person2.name}`);
+      break;
+    }
+
+    person2.damage();
+    if (person2.life == 0) {
+      console.log(`${text} ${person1.name}`);
+      break;
+    }
+  }
+}
+
+fight(fighter1, fighter2);
