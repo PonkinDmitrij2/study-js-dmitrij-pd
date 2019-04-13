8a743607cb5d2a2d12f14110071ce1f7b76f4dbc
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2019-04-13 14:11:37 +0300

    Adds formatNumber2 function (regexp version)

--- task-2/js/index.js
+++ task-2/js/index.js
@@ -4,3 +4,7 @@ function formatNumber(str) {
   const simbols = ['+', '(', ')', '-', ' '];
   return str.split('').filter(x => !simbols.includes(x)).slice(1).join('');
 }
+
+const formatNumber2 = str => {
+  return str.replace(/\+\d\((\d+)\)(\d+)-(\d+)-(\d+)/, '$1$2$3$4');
+};
